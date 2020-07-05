2e188da7a2426c78ded1f12780cc967a8b203d18
Fredi Benko <s52727@beuth-hochschule.de>
2020-07-05 21:54:00 +0200

    Codebasis

---
+++ test/Main.java
@@ -0,0 +1,20 @@
+package test;
+
+
+import main.Abonnement;
+
+/**
+ * Main erstellt ein Abonnement Objekt
+ *  und testet eine Funktion
+ */
+public class Main {
+    public static void main(String[] args) {
+	
+	Abonnement abo = new Abonnement("Peter", "Lustig", 1, 54.50);
+	System.out.println("Guthaben: " + abo.zeigeGuthaben());
+	System.out.println("Zahlung von 12.50");
+	abo.ausGuthabenZahlen(12.50, false);
+	System.out.println("Guthaben: " + abo.zeigeGuthaben());
+
+    }    
+}
